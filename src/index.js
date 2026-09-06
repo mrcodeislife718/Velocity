d2e1d1a389a2f0f117e4ca08c015acc09b5ae1a6
Charles Castillo <mrcodeislife718@proton.me>
2026-09-06 15:25:50 -0400

    Expose Velocity disaster recovery API

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ export * from './velocity.js';
 export * from './app.js';
 export * from './workflow.js';
 export * from './platform-projects.js';
+export { RecoveryStore, RecoveryCorruptionError } from './disaster-recovery.js';
